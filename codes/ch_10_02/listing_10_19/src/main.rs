bf3e575f9dd7d820cdc11a9507bb7c70e52c9089
ice <tony_mistark@163.com>
2023-05-17 21:45:27 +0800

    list 10_19

--- codes/ch_10_02/listing_10_19/src/main.rs
+++ codes/ch_10_02/listing_10_19/src/main.rs
@@ -1,8 +1,18 @@
+use std::any::type_name;
+
 fn main() {
     println!("Hello, world!");
     let string1 = String::from("abcd");
-    let string2 = String::from("xyz");
+    let string2 = "xyz";
+
+    println!("string1 type {}", type_of(&string1));
+    println!("string2 type {}", type_of(string2));
     
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+    // let result = longest(string1.as_str(), string2);
+    // println!("The longest string is {}", result);
+}
+
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
 }
