e3e2ca46f9ab6be9882111e3d2f4cf35143ca139
ice <tony_mistark@163.com>
2023-05-18 10:31:22 +0800

    ch_11_01 listing_11_1-5

---
+++ codes/ch_11_01/listing_11_1/adder/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+}
