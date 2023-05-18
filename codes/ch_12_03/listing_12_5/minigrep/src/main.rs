1f4536f22ead22c0f2c3e8ae42a8dc917a803496
ice <tony_mistark@163.com>
2023-05-18 21:52:16 +0800

    ch_12_03/listing_12_5/minigrep

---
+++ codes/ch_12_03/listing_12_5/minigrep/src/main.rs
@@ -0,0 +1,20 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let (query, file_path) = parse_config(&args);
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    println!("with text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
+}
