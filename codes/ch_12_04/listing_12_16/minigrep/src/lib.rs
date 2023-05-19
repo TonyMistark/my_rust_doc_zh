d07e9f6fbfcc8263f0f353ee034a2cce8ce55011
ice <tony_mistark@163.com>
2023-05-19 20:50:46 +0800

    ch_12_04

---
+++ codes/ch_12_04/listing_12_16/minigrep/src/lib.rs
@@ -0,0 +1,49 @@
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+   pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config{ query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    vec![]
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "
+Rust:
+safe, fast, productive.
+Pick three.";
+        // Note: search function not define yet.
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
