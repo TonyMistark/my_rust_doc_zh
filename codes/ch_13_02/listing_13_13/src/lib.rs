2902f2e10fc0987537ba5db393fb865082fcf083
ice <tony_mistark@163.com>
2023-05-23 20:24:10 +0800

    listing_13_13

---
+++ codes/ch_13_02/listing_13_13/src/lib.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+}
