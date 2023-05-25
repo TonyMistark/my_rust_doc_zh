da729097bb6a8a9be4ce9538a6c46873da9b4df6
ice <tony_mistark@163.com>
2023-05-25 22:01:18 +0800

    listing_15_9

---
+++ codes/ch_15/listing_15_9/src/main.rs
@@ -0,0 +1,16 @@
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    // 解引用尚未实现
+    assert_eq!(5, *y);
+}
