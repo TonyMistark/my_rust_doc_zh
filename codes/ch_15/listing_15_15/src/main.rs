f68428e56295a3ec23ee5189827dd47b3e408820
ice <tony_mistark@163.com>
2023-05-26 22:06:34 +0800

    listing_15_15

---
+++ codes/ch_15/listing_15_15/src/main.rs
@@ -0,0 +1,19 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    // do not allowed to free manually
+    // c.drop();
+    println!("CustomSmartPointer dropped before the end of main.");
+}
