22012f5abdab855541cce981176218b27000edc8
ice <tony_mistark@163.com>
2023-06-04 16:01:19 +0800

    listing_17_02: encapsulation

---
+++ codes/ch_17/listing_17_02/average_collection/src/lib.rs
@@ -0,0 +1,27 @@
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
