b9b6604fbf049dc18fca1c20af8eb3c5722d3d54
ice <tony_mistark@163.com>
2023-06-05 20:47:56 +0800

    listing_17_09: screen draw something

---
+++ codes/ch_17/listing_17_09/src/main.rs
@@ -0,0 +1,37 @@
+use gui::{Draw, Screen, Button};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+        println!("draw a select box, width: {}, height: {}", self.width, self.height);
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 5,
+                height: 8,
+                label: String::from("I am Buttun")
+            })
+        ],
+    };
+
+    screen.run();
+}
