cfca6b6e14159e9648f070eafc7101bd7454802e
ice <tony_mistark@163.com>
2023-06-07 20:52:18 +0800

    listing_19_07: segmentation fault

---
+++ codes/ch_19/listing_19_07/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    use std::slice;
+    let address = 0x01234usize;
+    let r = address as *mut i32;
+
+    let values: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+    println!("valuse: {:?}", values);
+}
