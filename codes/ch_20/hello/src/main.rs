32be589a6b17b9c335732212212a4b7d647a2fec
ice <tony_mistark@163.com>
2023-06-12 23:01:56 +0800

    refactor

--- codes/ch_20/hello/src/main.rs
+++ codes/ch_20/hello/src/main.rs
@@ -16,19 +16,19 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filenem) = if request_line == "GET / HTTP/1.1" {
+        println!("response with OK!");
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        println!("response with ERROR!");
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filenem).unwrap();
     let length = contents.len();
-
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
-    println!("response with OK!");
 }
 
