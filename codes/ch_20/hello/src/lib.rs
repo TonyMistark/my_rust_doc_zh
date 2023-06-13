1ff289c30c4e6c37d1451db45ed3b03a8c52fe34
ice <tony_mistark@163.com>
2023-06-13 22:38:05 +0800

    threads pool

---
+++ codes/ch_20/hello/src/lib.rs
@@ -0,0 +1,62 @@
+use std::{
+    thread, 
+    sync::{mpsc, Arc, Mutex},
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    /// create a threads pool
+    ///
+    /// size of the threads poll
+    ///
+    /// # Panics
+    ///
+    /// `new` fn will be panic, if size is 0
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        };
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+        {
+            let job = Box::new(f);
+            self.sender.send(job).unwrap();
+        }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; excuting...");
+            
+            job();
+        });
+
+        Worker {id, thread}
+    }
+}
